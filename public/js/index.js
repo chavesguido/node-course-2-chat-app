6481025c2b872e5c67993ec6d650c02921a2c4ea
chavesguido <chavesguido@hotmail.com>
2018-01-11 02:49:27 -0300

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,12 +2,6 @@ var socket = io();
 
 socket.on('connect', function () {
   console.log('Connected to server');
-
-  socket.emit('createMessage', {
-    from: 'Guido',
-    text: 'Todo bien, y vos?'
-  });
-
 });
 
 socket.on('disconnect', function () {
--- server/server.js
+++ server/server.js
@@ -15,14 +15,13 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
-  socket.emit('newMessage', {
-    from: 'Jorge',
-    text: 'Hola, como andas',
-    createdAt: 123
-  });
-
   socket.on('createMessage', (message) => {
     console.log('createMessage', message);
+    io.emit('newMessage', { //io.emit manda a todos los conectados. socket.emit manda solo a uno
+      from: message.from,
+      text: message.text,
+      createdAt: new Date().getTime()
+    });
   });
 
   socket.on('disconnect', () => {
